a7cd77b190ce7e726e2acad24bcc31418a6a44ca
imra jamil malik <ijmalik@gmail.com>
2012-11-13 07:46:31 +0500

    Finished layout and routes

--- example_user.rb
+++ example_user.rb
@@ -9,4 +9,13 @@ class User
 	def formatted_email
 		"#{@name} <#{@email}"
 	end
+  def string_shuffle(s)
+    s.split('').shuffle.join
+  end
 end
+
+  class String
+  	def shuffle
+  		self.split('').shuffle.join
+  	end
+  end
